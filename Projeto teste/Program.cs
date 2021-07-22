c137a07eb5022b3636acdaa5677e2b94af46ffa1
Marco Rodrigues <marco.rodrigues@pmm.am.gov.br>
2021-07-22 11:27:11 -0400

    tenha um bom dia!

--- Projeto teste/Program.cs
+++ Projeto teste/Program.cs
@@ -7,8 +7,8 @@ namespace Projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia!");
             Console.WriteLine("Como você está?");
+            Console.WriteLine("Tenha um bom dia!");
             Console.ReadLine();
 
         }
